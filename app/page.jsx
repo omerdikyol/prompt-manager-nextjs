672753d820618690de2a495d4fa0281a998de514
Ömer Dikyol <omerdikyol02@gmail.com>
2023-10-24 11:04:23 +0300

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Prompt Manager
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Share & Discover AI Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Prompt Manager is a community-driven platform for sharing and discovering AI prompts.
+      </p>
+    </section>
+  )
+}
+
+export default Home
